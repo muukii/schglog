3380fe4ac5fe3cc9f7ee448acf9fa846ecaba513
Muukii <muukii.app@gmail.com>
2022-03-22 19:37:21 +0900

    Add Command

--- Sources/schglog/main.swift
+++ Sources/schglog/main.swift
@@ -1,1 +1,12 @@
 import ArgumentParser
+
+struct Command: ParsableCommand {
+  
+  static var configuration: CommandConfiguration = .init(subcommands: [Gen.self])
+}
+
+struct Gen: ParsableCommand {
+  
+}
+
+Command.main()
